66400fa59f760ea629cc58324359bfe1ba4177fa
adrfrank <adrfrank@outlook.com>
2015-01-08 21:53:40 -0600

    Codigos agregados para monitorear el giroscopio y los electrodos

---
+++ giroscopio.py
@@ -0,0 +1,20 @@
+from emokit import emotiv
+import platform
+if platform.system() == "Windows":
+    import socket
+import gevent
+
+if __name__ == "__main__":
+  headset = emotiv.Emotiv(display_output=False)    
+  gevent.spawn(headset.setup)
+  gevent.sleep(0)
+  try:
+    while True:
+      packet = headset.dequeue()
+      print packet.gyro_x, packet.gyro_y
+      print packet.raw_data
+      gevent.sleep(0)
+  except KeyboardInterrupt:
+    headset.close()
+  finally:
+    headset.close()
